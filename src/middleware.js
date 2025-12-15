9a257c43b6798b422dc5a29462e11034c7ed9803
Raymond Camden <raymondcamden@gmail.com>
2025-12-15 09:10:14 -0600

    debug2

--- src/middleware.js
+++ src/middleware.js
@@ -3,7 +3,7 @@ export async function onRequest (context, next) {
     if not logged in, redirect
     */
     console.log(context.url.pathname)
-    if(context.url.pathname.endsWith('/app/') || context.url.pathname.endsWith('/app/callback')) return next();
+    if(context.url.pathname.endsWith('/app/') || context.url.pathname.endsWith('/app/callback') || context.url.pathname.endsWith('/app')) return next();
     console.log('need to check stuff', context.url.pathname);
     let token = await context.session.get('access_token');
     console.log('token', token);
