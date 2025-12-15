04bbeeec89274d25d1d0868a83e901659efdfa28
Raymond Camden <raymondcamden@gmail.com>
2025-12-15 08:34:04 -0600

    initial version

---
+++ src/middleware.js
@@ -0,0 +1,15 @@
+export async function onRequest (context, next) {
+    /*
+    if not logged in, redirect
+    */
+    console.log(context.url.pathname)
+    if(context.url.pathname.endsWith('/app/') || context.url.pathname.endsWith('/app/callback')) return next();
+    console.log('need to check shit');
+    let token = await context.session.get('access_token');
+    console.log('token', token);
+
+    if(!token) return context.redirect('/app');
+    context.locals.access_token = token;
+    
+    return next();
+};
